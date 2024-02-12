5d93ab06f9159ab60eb4f515ebf919624b0920a8
Den <denyszkramar@gmail.com>
2024-02-12 14:20:56 +0200

    feat: add controller, add request, add command for import in db

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use App\Models\Data;
 
 class ImportData extends Command
 {
@@ -18,13 +20,44 @@ class ImportData extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Import data from two json files';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $firstResponse = Http::get('https://submitter.tech/test-task/endpoint1.json');
+        $firstData = $firstResponse->json();
+
+        $secondResponse = Http::get('https://submitter.tech/test-task/endpoint2.json');
+        $secondData = $secondResponse->json();
+
+        foreach ($firstData as $firstItem) {
+            foreach ($secondData['data']['list'] as $secondItem) {
+                if ($firstItem['name'] == $secondItem['dimensions']['ad_id']) {
+
+                    Data::create([
+                        'ad_id' => $secondItem['dimensions']['ad_id'],
+                        'impressions' => $secondItem['metrics']['impressions'],
+                        'clicks' => $firstItem['clicks'],
+                        'unique_clicks' => $firstItem['unique_clicks'],
+                        'leads' => $firstItem['leads'],
+                        'conversion' => $secondItem['metrics']['conversion'],
+                        'roi' => $firstItem['roi'],
+                    ]);
+                }
+            }
+        }
+
+        $this->info('Data combined and imported successfully.');
+    }
+
+    public function calculateConversion($clicks, $leads) {
+        return $leads > 0 ? ($leads / $clicks) * 100 : 0;
+    }
+
+    public function calculateROI($clicks, $leads, $impressions) {
+        return $impressions > 0 ? (($leads * 100) / $impressions) - (($clicks * 100) / $impressions) : 0;
     }
 }
